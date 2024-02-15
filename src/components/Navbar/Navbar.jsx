e2f6adfe4bd9cc59df93adb060b764b679dfc0ff
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-02-15 20:37:37 +0530

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { CiUser } from "react-icons/ci";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaRegWindowClose } from "react-icons/fa";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-logo">
@@ -11,16 +20,33 @@ const Navbar = () => {
         <p>WWE</p>
       </div>
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      {/* <ul className="nav-links">
+          <li>Home</li>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul> */}
+
+      <div className={`nav-links ${isDrawerOpen ? "open" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button className="signup-login-mini">
+          <CiUser />
+          Sign Up
+        </button>
+      </div>
 
       <button className="signup-login">
         <CiUser />
         Sign Up
+      </button>
+
+      <button className="drawer-toggle" onClick={toggleDrawer}>
+        {isDrawerOpen ? <FaRegWindowClose /> : <RxHamburgerMenu />}
       </button>
     </nav>
   );
